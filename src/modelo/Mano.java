36bf2602d905af4c07fea580dcf6c4466660a0b1
Desarrollo <Desarrollo@DESKTOP-SRGB433.local>
2024-05-28 09:17:38 +0200

    Clase Mno modificada
    Modificado el método valorMano para que los ases puedan valer tanto 1
    como11, añadido el método finDeJjuego y toString

--- src/modelo/Mano.java
+++ src/modelo/Mano.java
@@ -9,10 +9,22 @@ public class Mano extends Mazo {
 		this.cartas = new ArrayList<Carta>();
 	}
 
-	public int valorMano() {
+	public int valorMano() throws Masde21Exception {
 		int valor=0;
+		int numAses=0;
 		for(Carta c : this.cartas) {
+			if (c.getNumero()==1) {
+				numAses++;
+			}
 			valor = valor + c.getValor();
+		}
+		// comprobamos si me he pasado de 21
+		if (numAses>0 && valor>21) {
+			valor=valor-10;
+			numAses--;
+		}
+		if(valor>20) {
+			throw new Masde21Exception();
 		}
 		return valor;
 	}
